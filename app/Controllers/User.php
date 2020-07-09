ab7a74594314b46b55efe33bcd4f684d9085b18a
tezaharfi <teza1800016062@webmail.uad.ac.id>
2020-07-09 22:12:32 +0700

    LOgin Form dengan CodeIgniter 4

---
+++ app/Controllers/User.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class User extends BaseController
+{
+	public function index()
+	{
+		return view('user_view');
+	}
+
+	//--------------------------------------------------------------------
+
+}
